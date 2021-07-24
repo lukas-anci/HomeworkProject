faf8bef0f7b198b5745ea231afff9d6d3b6d44a7
Lukas <lukas.ancikevicius@gmail.com>
2021-07-24 16:37:59 +0300

    cra andclean up

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  state = {};
+  render() {
+    return <div className="App">this is app</div>;
+  }
+}
+
+export default App;
